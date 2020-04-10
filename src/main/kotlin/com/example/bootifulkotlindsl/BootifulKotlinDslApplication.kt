9600186782aefac61aa95d913a2930a26e5da880
Marcel Widmer <marzelwidmer@gmail.com>
2020-04-10 10:00:26 +0200

    Initial Commit

---
+++ src/main/kotlin/com/example/bootifulkotlindsl/BootifulKotlinDslApplication.kt
@@ -0,0 +1,11 @@
+package com.example.bootifulkotlindsl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BootifulKotlinDslApplication
+
+fun main(args: Array<String>) {
+	runApplication<BootifulKotlinDslApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/bootifulkotlindsl/BootifulKotlinDslApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bootifulkotlindsl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BootifulKotlinDslApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
